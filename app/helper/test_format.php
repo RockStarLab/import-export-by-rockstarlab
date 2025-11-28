debea8180bb3228fa0b828ff843512d298dcb154
DK <12991525+DKudleichuk@users.noreply.github.com>
2025-11-28 13:58:02 +0200

    test: check PHP auto-formatting

---
+++ app/helper/test_format.php
@@ -0,0 +1,15 @@
+<?php
+namespace WP_AIE\helper;
+class test_format{
+public static function bad_format($a,$b){
+if($a>$b){
+return $a;
+}else{
+return $b;
+}
+}
+public static function another_method( $x, $y ) {
+$result=$x+$y;
+return $result;
+}
+}
